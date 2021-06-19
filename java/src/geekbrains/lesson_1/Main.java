6d960bf715bc31d17732616c9013f574b80f0dc5
NickVodolazov <86152289+NickVodolazov@users.noreply.github.com>
2021-06-19 16:55:39 +0500

    практика 1

---
+++ java/src/geekbrains/lesson_1/Main.java
@@ -0,0 +1,10 @@
+package geekbrains.lesson_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
